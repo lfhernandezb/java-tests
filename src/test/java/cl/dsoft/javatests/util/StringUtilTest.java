d2d744f6f7e3778668e530d6faaf159833a986d0
Luis Hernandez <lfhernandez@MacBook-Pro-de-Luis.local>
2024-02-06 22:36:54 -0300

    test unitario usando Assert de JUnit; se usa @Test en nuestra funcion

--- src/test/java/cl/dsoft/javatests/util/StringUtilTest.java
+++ src/test/java/cl/dsoft/javatests/util/StringUtilTest.java
@@ -2,34 +2,13 @@ package cl.dsoft.javatests.util;
 
 // import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
-        //System.out.println(result);
-        /*
-        if (!result.equals("holaholahola")) {
-            //System.out.println("ERROR");
-            throw new RuntimeException("Error");
-        }
-        */
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
-
-        String result2 = StringUtil.repeat("hola", 1);
-        //System.out.println(result2);
-        /*
-        if (!result2.equals("hola")) {
-            //System.out.println("ERROR");
-            throw new RuntimeException("Error");
-         }
-         */
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
-
+    @Test
+    public void testRepeat() {
+        Assert.assertEquals( "holaholahola", StringUtil.repeat("hola", 3));
+        Assert.assertEquals( "hola", StringUtil.repeat("hola", 1));
     }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!expected.equals(actual)) {
-            throw new RuntimeException(actual + " is not equal to " + expected);
-        }
-    }
-
-
 }
