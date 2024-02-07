50f588a206f224540f0a3458b35060424e000a36
Luis Hernandez <lfhernandez@MacBook-Pro-de-Luis.local>
2024-02-06 23:03:09 -0300

    test unitario usando cada prueba en una función distinta; el nombre de la funcion describe la prueba

--- src/main/java/cl/dsoft/javatests/util/StringUtil.java
+++ src/main/java/cl/dsoft/javatests/util/StringUtil.java
@@ -2,9 +2,13 @@ package cl.dsoft.javatests.util;
 
 public class StringUtil {
     public static String repeat(String str, int times) {
+
+        if (times < 0) {
+            throw new IllegalArgumentException("negative times not allowed");
+        }
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i <= times; i++) {
+        for (int i = 0; i < times; i++) {
             result.append(str);
         }
 
